eb3bb8f3b86482496f3cc3c4061bc30a26d72480
Hasibur Rahman <hasibur2060@gmail.com>
2026-05-06 22:04:51 +0600

    Dtos added

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,9 +2,9 @@
 {
     public class Product
     {
-        public required int Id { get; set; }
-        public required string Name { get; set; }
+        public  int Id { get; set; }
+        public  string Name { get; set; }
         public string? Description { get; set; }
-        public required decimal Price { get; set; }
+        public  decimal Price { get; set; }
     }
 }
