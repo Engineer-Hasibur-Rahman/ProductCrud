89832105afeafa4aef9ac59bf48ff0d7771af54b
Hasibur Rahman <hasibur2060@gmail.com>
2026-05-07 02:12:35 +0600

    category added

---
+++ DTOs/CategoryUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductCrud.DTOs
+{
+    public class CategoryUpdateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
